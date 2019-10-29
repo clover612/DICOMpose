6e1ddddd32f48a8838e91ba520c2013cf0cdc691
Srinidhi-Bharadwaj <sb3784@columbia.edu>
2019-10-29 16:23:34 -0400

    Added infrastructure for subnavigation

--- mass_html_singlept.sh
+++ mass_html_singlept.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 set -e
-htmlloc=/Volumes/Srinidhi_Bh/Provenzano/trial/CD3/summary.html; 
+htmlloc=/Volumes/Srinidhi_Bh/Provenzano/trial/CD2/summary.html; 
 touch $htmlloc; rm $htmlloc
 cat ~/Documents/Provenzano/DICOMpose/template_top.html >> $htmlloc;
-niifilepaths=$(find /Volumes/Srinidhi_Bh/Provenzano/trial/CD3/8302035 -name "*.nii*")
-outputdir=/Volumes/Srinidhi_Bh/Provenzano/trial/CD3
+niifilepaths=$(find /Volumes/Srinidhi_Bh/Provenzano/trial/CD2/PAT00000 -name "*.nii*"|sort)
+outputdir=/Volumes/Srinidhi_Bh/Provenzano/trial/CD2
 oldPROTNAME=0
 
 while read -r img 
@@ -16,19 +16,26 @@ do
     img2=$(echo ${foo##*/})
     PROTNAME=$(cut -d'_' -f1 <<<$img2)
     if [ "$PROTNAME" != "$oldPROTNAME" ]; then
-  		sed -i '' '/div>checksrin/i \ 
-  		<a href="#PROTNAME">PROTNAME</a> \
-  		' "$htmlloc"; 
+        sed -i '' 's/emma//g' $htmlloc
+  		# sed -i '' '/div>checksrin/i \ 
+  		# <a href="#PROTNAME">PROTNAME</a> \
+  		# ' "$htmlloc"; 
+        sed -i '' '/div>checksrin/i \ 
+        potato \
+        ' "$htmlloc";
+
+        sed -i '' '/potato/{
+        s/potato//g
+        r /Users/srinidhibharadwaj/Documents/Provenzano/DICOMpose/subnav_temp.html
+        }' $htmlloc
+
+        echo "<h1 style=\"color:green;\">$PROTNAME</h1>">>$htmlloc
+
 		sed -i -e "s^PROTNAME^$PROTNAME^g" "$htmlloc";
-		if [ "$oldPROTNAME" != "0" ]; then
-			echo "</div>" >> $htmlloc
-		fi
-		echo "<div id=\"$PROTNAME\">" >> $htmlloc
-		echo "<h1>$PROTNAME</h1>">>$htmlloc
     fi
 
     cat ~/Documents/Provenzano/DICOMpose/template_wt.html >> $htmlloc;
-    sed -i -e "s^IMGNAME^$img2^g" "$htmlloc";
+    sed -i -e "s^IMGNAME^$img2^g" "$htmlloc"; IMGLIST+=("$img2"); 
     sed -i -e "s^IMGLOC^$img^g" "$htmlloc";
     sed -i -e "s^PROTNAM^$PROTNAME^g" "$htmlloc";
     sed -i -e "s^PNGSOURCE^$outputdir\/summary_pngs\/$img2.png^g" "$htmlloc";
@@ -41,11 +48,17 @@ do
     fovx=`echo "$sizex * $nx" | bc -l`; sed -i -e "s^fovx^$fovx^g" "$htmlloc" ;
     fovy=`echo "$sizey * $ny" | bc -l`; sed -i -e "s^fovy^$fovy^g" "$htmlloc" ;
     fovz=`echo "$sizez * $nz" | bc -l`; sed -i -e "s^fovz^$fovz^g" "$htmlloc" ;
+    sed -i '' '/div>emma/i \ 
+    <a href="#img2">img2</a> \
+    ' "$htmlloc"; 
+    sed -i -e "s^img2^$img2^g" "$htmlloc";
     oldPROTNAME=$PROTNAME
 
 done <<< "$niifilepaths"
 
 echo "</div>" >> $htmlloc
+cat ~/Documents/Provenzano/DICOMpose/scripts.html >> $htmlloc;
 echo "</body>" >> $htmlloc
 echo "</html>" >> $htmlloc
 sed -i '' 's/checksrin//g' $htmlloc
+sed -i '' 's/emma//g' $htmlloc
